d410d0626a52d34c3d98f95dac59087a2f2d61cc
Dibakar khamrui <dibakar.ridzenetrprise@gmail.com>
2024-04-18 11:43:03 +0530

    search fixed

--- app/Http/Controllers/Admin/Remove/RemoveController.php
+++ app/Http/Controllers/Admin/Remove/RemoveController.php
@@ -8,7 +8,7 @@ use App\Models\Query;
 use App\Models\Booking;
 use App\Models\Quoted;
 use Carbon\Carbon;
-use DataTables,DB;
+use Yajra\DataTables\Facades\DataTables;
 
 class RemoveController extends Controller
 {
@@ -20,7 +20,6 @@ class RemoveController extends Controller
             $query = Query::select(
                 'id',
                 'prefix_quoteid',
-				DB::raw("CONCAT(queries.full_name,'-',queries.email,'-',queries.phone,'-',queries.mobile) as user_details"),
                 'full_name',
                 'email',
                 'phone',
@@ -32,32 +31,33 @@ class RemoveController extends Controller
                 return $query->select(
                     'id',
                     'query_id',
-                    DB::raw("CONCAT(bookings.booking_pickupPoint,'-',bookings.booking_postcode) as pickup_point"),
                     'booking_pickupPoint',
                     'booking_postcode',
                     'pick_datetime',
-					DB::raw("CONCAT(bookings.destination,'-',bookings.destination_postcode) as destination"),
                     'destination',
 					'destination_postcode'
                 );
             }]);
             $data = $query->onlyTrashed()->latest('id');
-		    return Datatables::of($data)->addIndexColumn()
-				->filterColumn('user_details', function($query, $keyword) {
-                    $sql = "CONCAT(queries.full_name,'-',queries.email,'-',queries.phone,'-',queries.mobile)  like ?";
-                    $query->whereRaw($sql, ["%{$keyword}%"]);
-                })
-                ->filterColumn('pickup_point', function($query, $keyword) {
-                    $query->whereHas('booking', function($query) use ($keyword){
-                        $sql = "CONCAT(bookings.booking_pickupPoint,'-',bookings.booking_postcode)  like ?";
-                        $query->whereRaw($sql, ["%{$keyword}%"]);
-                    });
-                })
-                ->filterColumn('destination', function($query, $keyword) {
-                    $query->whereHas('booking', function($query) use ($keyword){
-                        $sql = "CONCAT(bookings.destination,'-',bookings.destination_postcode)  like ?";
-                        $query->whereRaw($sql, ["%{$keyword}%"]);
-                    });
+		    return DataTables::of($data)->addIndexColumn()
+                ->filter(function ($instance) use ($request) {
+                    if (!empty($request['search']['value'])) {
+                        $search = $request['search']['value'];
+                        $instance->where(function($query) use($search){
+                            $query->orWhere('email', 'LIKE', "%{$search}%")
+                            ->orWhere('full_name', 'LIKE', "%{$search}%")
+                            ->orWhere('id', 'LIKE', "%{$search}%")
+                            ->orWhere('mobile', 'LIKE', "%{$search}%")
+                            ->orWhere('prefix_quoteid', 'LIKE', "%{$search}%")
+                            ->orWhere('phone', 'LIKE', "%{$search}%")
+                            ->orWhereHas('booking', function ($queryHas) use ($search) {
+                                $queryHas->where('booking_pickupPoint', 'LIKE', "%{$search}%")
+                                ->orWhere('booking_postcode', 'LIKE', "%{$search}%")
+                                ->orWhere('destination', 'LIKE', "%{$search}%")
+                                ->orWhere('destination_postcode', 'LIKE', "%{$search}%");
+                            });
+                        });
+                    }
                 })
                 ->addColumn('quote_id', function ($data) {
                     return ($data->prefix_quoteid.''.$data->booking->query_id);
@@ -70,9 +70,6 @@ class RemoveController extends Controller
                     $user .= '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
 <path fill-rule="evenodd" clip-rule="evenodd" d="M17.3545 22.2323C15.3344 21.7262 11.1989 20.2993 7.44976 16.5502C3.70065 12.8011 2.2738 8.66559 1.76767 6.6455C1.47681 5.48459 2.00058 4.36434 2.88869 3.72997L5.21694 2.06693C6.57922 1.09388 8.47432 1.42407 9.42724 2.80051L10.893 4.91776C11.5152 5.8165 11.3006 7.0483 10.4111 7.68365L9.24234 8.51849C9.41923 9.1951 9.96939 10.5846 11.6924 12.3076C13.4154 14.0306 14.8049 14.5807 15.4815 14.7576L16.3163 13.5888C16.9517 12.6994 18.1835 12.4847 19.0822 13.1069L21.1995 14.5727C22.5759 15.5257 22.9061 17.4207 21.933 18.783L20.27 21.1113C19.6356 21.9994 18.5154 22.5232 17.3545 22.2323ZM8.86397 15.136C12.2734 18.5454 16.0358 19.8401 17.8405 20.2923C18.1043 20.3583 18.4232 20.2558 18.6425 19.9488L20.3056 17.6205C20.6299 17.1665 20.5199 16.5348 20.061 16.2171L17.9438 14.7513L17.0479 16.0056C16.6818 16.5182 16.0047 16.9202 15.2163 16.7501C14.2323 16.5378 12.4133 15.8569 10.2782 13.7218C8.1431 11.5867 7.46219 9.7677 7.24987 8.7837C7.07977 7.9953 7.48181 7.31821 7.99439 6.95208L9.24864 6.05618L7.78285 3.93893C7.46521 3.48011 6.83351 3.37005 6.37942 3.6944L4.05117 5.35744C3.74413 5.57675 3.64162 5.89565 3.70771 6.15943C4.15989 7.96418 5.45459 11.7266 8.86397 15.136Z" fill="#7e7e7e"/>
 </svg>'.$data->phone.' <br>';
-                    $user .= '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
-<path d="M11 18H13M9.2 21H14.8C15.9201 21 16.4802 21 16.908 20.782C17.2843 20.5903 17.5903 20.2843 17.782 19.908C18 19.4802 18 18.9201 18 17.8V6.2C18 5.0799 18 4.51984 17.782 4.09202C17.5903 3.71569 17.2843 3.40973 16.908 3.21799C16.4802 3 15.9201 3 14.8 3H9.2C8.0799 3 7.51984 3 7.09202 3.21799C6.71569 3.40973 6.40973 3.71569 6.21799 4.09202C6 4.51984 6 5.07989 6 6.2V17.8C6 18.9201 6 19.4802 6.21799 19.908C6.40973 20.2843 6.71569 20.5903 7.09202 20.782C7.51984 21 8.07989 21 9.2 21Z" stroke="#7e7e7e" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-</svg>'.$data->mobile.' <br>';
                     $user .= '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
 <path d="M21 8L17.4392 9.97822C15.454 11.0811 14.4614 11.6326 13.4102 11.8488C12.4798 12.0401 11.5202 12.0401 10.5898 11.8488C9.53864 11.6326 8.54603 11.0811 6.5608 9.97822L3 8M6.2 19H17.8C18.9201 19 19.4802 19 19.908 18.782C20.2843 18.5903 20.5903 18.2843 20.782 17.908C21 17.4802 21 16.9201 21 15.8V8.2C21 7.0799 21 6.51984 20.782 6.09202C20.5903 5.71569 20.2843 5.40973 19.908 5.21799C19.4802 5 18.9201 5 17.8 5H6.2C5.0799 5 4.51984 5 4.09202 5.21799C3.71569 5.40973 3.40973 5.71569 3.21799 6.09202C3 6.51984 3 7.07989 3 8.2V15.8C3 16.9201 3 17.4802 3.21799 17.908C3.40973 18.2843 3.71569 18.5903 4.09202 18.782C4.51984 19 5.07989 19 6.2 19Z" stroke="#7e7e7e" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>'.$data->email;
--- resources/views/backend/removed/index.blade.php
+++ resources/views/backend/removed/index.blade.php
@@ -76,12 +76,6 @@
             var table = $('#dataTable').DataTable({
                 processing: true,
                 serverSide: true,
-                order: [[ 0 , "desc" ]],
-                pageLength: 15,
-                lengthMenu: [
-                    [15, 25, 50, -1],
-                    [15, 25, 50, 'All']
-                ],
                 columnDefs: [ { orderable: false, targets: [0,1,2,3,4,5] ,} ],
                 language: {
                         processing: '<div id="resultLoading"><div><i style="font-size: 46px;color: #363062;" class="fa fa-spinner fa-spin fa-2x fa-fw" aria-hidden="true"></i></div><div class="bg"></div></div>'
@@ -89,12 +83,12 @@
                 ajax: "{{ route('admin.removed.index') }}",
                 
                 columns: [
-                     {data: 'id', name: 'id', visible:false },
-					{data: 'quote_id', name: 'quote_id', orderable: false},
-					{data: 'user_details', name: 'user_details', orderable: false},
-					{data: 'pickup_point', name: 'pickup_point', orderable: false},
-					{data: 'pickup_datetime', name: 'pickup_datetime', orderable: false},
-					{data: 'destination', name: 'destination', orderable: false},
+                    { data: 'DT_RowIndex', orderable: false, searchable: false,visible: false },
+					{data: 'quote_id', name: 'quote_id'},
+					{data: 'user_details', name: 'user_details'},
+					{data: 'pickup_point', name: 'pickup_point'},
+					{data: 'pickup_datetime', name: 'pickup_datetime'},
+					{data: 'destination', name: 'destination'},
 					{data: 'action', name: 'action', orderable: false, searchable: true}
                 ],
             });
